459c5d62b81fc5522b69ed177e962bad327b0086
Ali Raza Ashraf <adaydreamer10@gmail.com>
2024-03-05 22:13:25 +0100

    fetching data from api and finished fetching and showing content of the word on front-end

---
+++ script.js
@@ -0,0 +1,41 @@
+// console.log(
+//     fetch('https://api.dictionaryapi.dev/api/v2/entries/en_US/computer')
+//     .then(response => response.json())
+//     .then(data => console.log(data))
+// )
+
+const url = 'https://api.dictionaryapi.dev/api/v2/entries/en/'
+
+const outputBox = document.getElementById('output-box')
+const sound = document.getElementById('sound')
+const searchBtn = document.getElementById('search-btn')
+
+
+
+
+searchBtn.addEventListener('click', () => {
+
+    let inputWord = document.getElementById('input-word').value
+
+        fetch(`${url}${inputWord}`)
+        .then(response => response.json())
+        .then(data => {
+            // console.log(data)
+            outputBox.innerHTML = `
+            <div class="word" id="word">
+            <h3> ${inputWord} </h3>
+            <!-- font awesome speaker icon -->
+            <button>
+            <i class="fas fa-volume-up"></i>
+            </button>
+        </div>
+        <div class="details">
+            <p>${data[0].meanings[data[0].meanings.length - 1].partOfSpeech}</p>
+            <p>/${data[0].phonetics[data[0].phonetics.length - 1].text}/</p>
+        </div>
+        <p class="word-meaning">${data[0].meanings[0].definitions[0].definition}</p>
+        <p class="word-example">${data[0].meanings[0].definitions[0].example || ""}</p>`
+        })
+})
+
+
